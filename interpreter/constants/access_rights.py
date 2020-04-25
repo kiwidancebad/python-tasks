2d4c036a37bc22a4e5dc40da042b27aa8558071e
theredbe <morelessequal@gmail.com>
2020-04-25 22:32:33 +0300

    feat: constants

---
+++ interpreter/constants/access_rights.py
@@ -0,0 +1,18 @@
+ACCESS_RIGHTS = [
+  {
+    'code': 'r',
+    'description': 'read'
+  },
+  {
+    'code': 'w',
+    'description': 'write'
+  },
+  {
+    'code': 'o',
+    'description': 'own'
+  },
+  {
+    'code': 'x',
+    'description': 'execute'
+  }
+]
---
+++ interpreter/constants/commands.py
@@ -0,0 +1,26 @@
+COMMANDS = [
+  {
+    'name': 'create object',
+    'code': 'cco'
+  },
+  {
+    'name': 'create subject',
+    'code': 'ccs'
+  },
+  {
+    'name': 'destroy object',
+    'code': 'cdo'
+  },
+  {
+    'name': 'destroy subject',
+    'code': 'cds'
+  },
+  {
+    'name': 'enter p into matrix',
+    'code': 'cer'
+  },
+  {
+    'name': 'delete p from matrix',
+    'code': 'cdr'
+  }
+]
