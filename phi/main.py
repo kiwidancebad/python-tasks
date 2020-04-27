9c06b7564e404be29670a72f9c649440d2cc4325
theredbe <morelessequal@gmail.com>
2020-04-27 14:11:31 +0300

    feat: phi function

---
+++ phi/main.py
@@ -0,0 +1,25 @@
+from utils.phi import phi
+
+testArr = [
+  1245,
+  7544,
+  7935,
+  6278,
+  3405,
+  4176,
+  5972,
+  9045,
+  6712,
+  5972,
+  6735,
+  7914,
+  7842,
+  6375,
+  9145,
+  3366,
+  7912,
+  6734,
+]
+
+for item in testArr:
+  print("Euler's totient function for %s: "%(item) + str(phi(item)))
