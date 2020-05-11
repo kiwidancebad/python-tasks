accbc2405933ff678621ed7b72a69d7e87c2d8f7
theredbe <morelessequal@gmail.com>
2020-05-11 14:12:33 +0300

    fix: exponential squaring for loop

--- misc/exponential_squaring.py
+++ misc/exponential_squaring.py
@@ -2,7 +2,7 @@ def power(g_base, a, p_mod):
   x=1
   bits = "{0:b}".format(a)
 
-  for i, bit in enumerate(bits):
+  for _, bit in enumerate(bits):
     if bit == '1': x = (((x**2) * g_base) % p_mod)
     elif bit == '0': x = ((x**2) % p_mod)
 
